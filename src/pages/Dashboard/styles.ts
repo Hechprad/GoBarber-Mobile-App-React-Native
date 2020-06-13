4fefd8039fa4705f11be99b9e24b4025e473da5a
Jorge Hecherat <hecherat@gmail.com>
2020-06-13 12:25:22 -0300

    [develop] - Button position fix

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
 
 export const Button = styled.Button``;
